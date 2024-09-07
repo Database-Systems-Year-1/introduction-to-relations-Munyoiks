91302cb5f036d1838ad2b01633ffef2860c2d66f
Munyoike Ian <167209005+Munyoiks@users.noreply.github.com>
2024-09-07 16:57:46 +0300

    Add files via upload

---
+++ Munyoike's Assignment.sql
@@ -0,0 +1,41 @@
+CREATE TABLE footballers(
+name VARCHAR(255),
+ID int,
+position VARCHAR(255)
+);
+
+INSERT INTO footballers(name,ID,position)
+VALUES ('Cristiano',7, 'striker'),
+       ('Messi', 10, 'right winger'),
+	   ('Dybala',23,' attacking midfielder'),
+	   ('Pogba',6, 'midfielder');
+ALTER TABLE footballers
+ADD COLUMN team VARCHAR(255);
+
+UPDATE footballers
+SET team = 'Madrid'
+WHERE name  = 'Cristiano';
+
+UPDATE footballers
+SET team = 'Barcelona'
+WHERE name = 'Messi';
+
+UPDATE footballers
+SET team = 'Juventus'
+WHERE name = 'Dybala';
+
+UPDATE footballers
+SET team = 'Manchester'
+WHERE name = 'Pogba';
+
+ALTER table footballers
+ALTER COLUMN position TYPE VARCHAR(100);
+
+DELETE from footballers
+WHERE name = 'Messi';
+
+ALTER TABLE footballers
+DROP COLUMN ID;
+
+SELECT * FROM footballers;
+DROP TABLE footballers;
